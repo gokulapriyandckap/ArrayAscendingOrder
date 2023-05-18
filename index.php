d3c094dbbd373cfde7bacd3f2b4400756a9e1061
gokulapriyandckap <bgokulapriyandckap@gmail.com>
2023-05-18 17:11:58 +0530

    This is so far i tried in office

--- index.php
+++ index.php
@@ -1,32 +1,38 @@
 <?php
 // timestamp: 3:00
-//$array = [5, 2, 1];
-//$array = [1, 2, 5];
-$array = [4, 1, 2, 5];
+// $array = [5, 2, 1];
+$array = [1, 2, 5];
+echo isDescendingOrder($array);
+// $array = [4, 1, 2, 5];
 $result = [];
 if (isDescendingOrder($array)) {
     echo "The array is already arranged in descending order.";
 } else {
     do {
-        for ($i = 0; $i < count($array) - 1; $i++) {
-            if ($array[$i] < $array[$i + 1]) {
-                $temp = $array[$i + 1];
-                $array[$i + 1] = $array[$i];
-                $array[$i] = $temp;
+        for ($i =  count($array) - 1; $i = 0; $i--) {
+            // echo $i;
+            if ($array[$i] > $array[$i - 1]) {
+                $temp = $array[$i];
+                // $temp = $array[$i - 1];
+                $array[$i] = $array[$i - 1];
+                $array[$i - 1] = $temp;
+                // $array[$i - 1] = $array[$i];
                 // $result[] = $array;
                 //
                 array_push($result, $array);
             }
         }
-    } while (! isDescendingOrder($array));
+    }
+     while (isDescendingOrder($array));
     print_r($result);
 }
 function isDescendingOrder($array)
 {
     for ($i = 0; $i < (count($array) - 1); $i++) {
-        if ($array[$i] < $array[$i + 1]) {
-            return false;
+        if ($array[$i] > $array[$i + 1]) {
+            return true;
         }
     }
-    return true;
+    return false;
 }
+
